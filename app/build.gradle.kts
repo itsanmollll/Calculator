77ccae3249c41410a1428dc077fa327faec61642
Anmol Bhardwaj <anmolbhardwajinv224@gmail.com>
2023-10-14 11:27:24 +0530

    Some changes in Gradle and some in Kotlin file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,12 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    buildFeatures{
+
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,8 +43,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("net.object hunter:exp4j:0.4.8")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    implementation("androidx.viewbinding:viewbinding:1.0.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.calculator
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputBinding
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    var lastNumeric = false
+
+    var stateError = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    fun onEqualClick(view: View) {}
+
+
+    fun onDigitClick(view: View) {}
+
+
+    fun onAllclearClick(view: View) {}
+
+
+    fun onOperatorClick(view: View) {}
+
+
+    fun onClearClick(view: View) {}
+
+
+    fun onBackClick(view: View) {}
+
+
 }
