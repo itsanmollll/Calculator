5d029151b273f7090b6b10fae5761aafc3728764
Anmol Bhardwaj <anmolbhardwajinv224@gmail.com>
2023-10-14 12:31:37 +0530

    Some Backend

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.calculator
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputBinding
 import com.example.calculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,11 @@ class MainActivity : AppCompatActivity() {
     var lastNumeric = false
 
     var stateError = false
+
+    var lastDot = false
+
+    private lateinit var expression: Expression
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -38,5 +46,32 @@ class MainActivity : AppCompatActivity() {
 
     fun onBackClick(view: View) {}
 
+    fun oEqual(){
+
+        if (lastNumeric && !stateError){
+
+            val txt = binding.dataTv.text.toString()
+
+            expression = ExpressionBuilder(txt).build()
+
+            try {
+                val result = expression.evaluate()
+
+                binding.resultTv.visibility = View.VISIBLE
+
+                binding.resultTv.text = "=" + result.toString()
+            }catch (ex : ArithmeticException){
+
+                Log.e("evaluate error" , ex.toString())
+                binding.resultTv.text = "Error"
+                stateError = true
+                lastNumeric = false
+            }
+
+        }
+
+
+
+    }
 
 }
